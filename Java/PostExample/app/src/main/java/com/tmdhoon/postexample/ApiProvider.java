d4522d98ee5061799bf548eb59fd39e2d2797ddd
Tmdhoon2 <hks026naver@gmail.com>
2022-07-01 23:14:55 +0900

    설명추가

--- Java/PostExample/app/src/main/java/com/tmdhoon/postexample/ApiProvider.java
+++ Java/PostExample/app/src/main/java/com/tmdhoon/postexample/ApiProvider.java
@@ -3,17 +3,17 @@ package com.tmdhoon.postexample;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class ApiProvider {
-    private static Retrofit retrofit;
-    private static String BASE_URL = "http://13.125.227.67:8080";
+public class ApiProvider {                                                      // ApiProvider 클래스 생성
+    private static Retrofit retrofit;                                           // Retrofit 필드를 선언한다
+    private static String BASE_URL = "http://13.125.227.67:8080";               // baseurl 필드 선언
 
-    public static Retrofit getRetrofit() {
-        if(retrofit==null){
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+    public static Retrofit getRetrofit() {                                      // 모든 클래스나 패키지에서 getRetrofit 함수에 접근 가능하게 만든다
+        if(retrofit==null){                                                     // Retrofit 이 null 일때,
+            retrofit = new Retrofit.Builder()                                   // Retrofit Builder 를 통해 레트로핏을 생성한다
+                    .baseUrl(BASE_URL)                                          // baseurl은 위에서 가져오고
+                    .addConverterFactory(GsonConverterFactory.create())         // addConverterFactory를 통해 json 형식의 파일 내용을 받아옴
+                    .build();                                                   // 위에서 선언한 것들을 이용하여 레트로핏 생성
         }
-        return retrofit;
+        return retrofit;                                                        // 레트로핏 반환
     }
 }
--- Java/PostExample/app/src/main/java/com/tmdhoon/postexample/PostResponse.java
+++ Java/PostExample/app/src/main/java/com/tmdhoon/postexample/PostResponse.java
@@ -3,19 +3,19 @@ package com.tmdhoon.postexample;
 import android.text.Editable;
 
 public class PostResponse {
-    private Editable id;
-    private String title;
+    private Editable id;                                                                            // 접근제어수식어를 통해 같은 클래스에서만 접근이
+    private String title;                                                                           // 가능한 필드 생성
     private String content;
 
-    public Editable getId() {
+    public Editable getId() {                                                                       // Id는 입력받기 때문에 Editable 로 선언
         return id;
     }
 
-    public String getTitle() {
+    public String getTitle() {                                                                      // title 문자열 형식으로 받아옴
         return title;
     }
 
-    public String getContent() {
+    public String getContent() {                                                                    // 게시글의 내용을 불러옴
         return content;
     }
 }
